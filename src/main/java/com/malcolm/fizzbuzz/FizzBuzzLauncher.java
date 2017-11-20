d4b815609d5700c4836fc66d96bb5340dd66ead2
malcovitch <malcolm.d.bailey@gmail.com>
2017-11-20 15:26:35 +0000

    Update FizzBuzzLauncher.java

--- src/main/java/com/malcolm/fizzbuzz/FizzBuzzLauncher.java
+++ src/main/java/com/malcolm/fizzbuzz/FizzBuzzLauncher.java
@@ -2,8 +2,6 @@ package com.malcolm.fizzbuzz;
 
 public class FizzBuzzLauncher {
 
-    private FizzBuzzProcessor fizzBuzzProcessor;
-
     public void go(int lowerLimit, int upperLimit) {
        FizzBuzzProcessor fizzBuzzProcessor = FizzBuzzProcessorFactory.getFizzBuzzProcessor(FizzBuzzProcessorFactory.FizzBuzzProcessorType.LUCKY);
        for (int i = lowerLimit; i <= upperLimit; i++) {
